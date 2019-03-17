b9e81a73ee857d4ca9fc3fe9e21414e01dc439e7
Alexei Sorokine <2picae@gmail.com>
2019-03-17 14:26:24 -0700

    Add QuickStats

---
+++ src/components/QuickStats.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class QuickStats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className='quickstats-wrapper'>
+        <h2>QuickStats</h2>
+      </div>
+    );
+  }
+}
+
+export default QuickStats;
