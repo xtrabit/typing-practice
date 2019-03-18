414a631867c547c784fb08e3ce5d1d8491da2e2a
Alexei Sorokine <2picae@gmail.com>
2019-03-18 08:02:36 -0700

    Implement basic exercise

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -8,25 +8,38 @@ class Exercise extends React.Component {
     this.state = {
       colorPressed: 'lightgreen',
       colorNormal: 'none',
-      testString: ''
+      colorUsed: '#DEB887',
+      testString: [],
+      index: 0,
+      test: false,
+      next: false
     };
     this.startExercise = this.startExercise.bind(this);
   }
 
   startExercise() {
+    // let node = document.getElementById('exercise');
+    // console.log('NODE ', node)
+    // node.innerHTML = '';
     const test = loadTest(this.props.data);
-    this.setState({testString: test});
+    this.setState({testString: test, index: 0});
     let time = 0;
     let start = 0;
     let end = 0;
     let current = null;
+
     const handleKey = (e) => {
+      if (this.state.next && e.type === 'keydown') {
+        this.setState({next: false});
+        this.startExercise();
+        return;
+      }
+      let index = this.state.index;
       const key = e.key;
-      console.log(e.type);
-      console.log(key);
       if (key === 'Escape') {
         window.removeEventListener('keydown', handleKey);
         window.removeEventListener('keyup', handleKey);
+        this.setState({test: false});
       }
       if  (e.type === 'keyup') {
         start = new Date();
@@ -44,19 +57,53 @@ class Exercise extends React.Component {
             }, 250);
           })(current);
         }
+        console.log('CURRENT KEY - ', key)
+        let correctKey = this.state.testString[this.state.index];
+        let letter = document.getElementById('index' + this.state.index + correctKey);
+        console.log('index: ', this.state.index, '; key: ', key, '; expected key: ', correctKey)
+        if (key === this.state.testString[this.state.index]) {
+          letter.className = 'used';
+          // letter.style.cssText = `background-color: ${this.state.colorUsed}`;
+          index++;
+          if (index === this.state.testString.length) {
+            this.setState({next: true});
+            return;
+          }
+          correctKey = this.state.testString[index];
+          letter = document.getElementById('index' + index + correctKey);
+          letter.className = 'cursor';
+          this.setState({index: index});
+        } else {
+          console.log('index - ', index)
+          letter.className = 'mistake';
+        }
       }
     };
+    if (!this.state.test) {
+      window.addEventListener('keydown', handleKey.bind(this));
+      window.addEventListener('keyup', handleKey.bind(this));
+      this.setState({test: true});
+    }
+  }
 
-    console.log('add event listner')
-    window.addEventListener('keydown', handleKey);
-    window.addEventListener('keyup', handleKey);
+  displayString(flag) {
+    if (!flag) return null;
+    return this.state.testString.map((item, i) => {
+      return <span
+        key={'k' + item + i}
+        id={'index' + i + item}
+        className={i === 0 ? 'cursor' : 'regular'}
+        >
+        {item}
+        </span>
+    });
   }
 
   render() {
     return (
       <div className='exercise-wrapper'>
-        <div className='exercise' onClick={this.startExercise}>
-
+        <div id='exercise' className='exercise' onClick={this.startExercise} >
+          {this.displayString(this.state.test)}
         </div>
       </div>
     );
--- src/components/loadTest.js
+++ src/components/loadTest.js
@@ -1,6 +1,28 @@
+const {letters} = require('./charSet.js');
+
 const loadTest = (data) => {
   console.log(data);
+  return generateString();
+};
 
+const generateString = () => {
+  let min = 3;
+  let max = 7;
+  let str = [];
+  let length = 15;
+  let total = 0;
+  while(total < length) {
+    let qty = ~~(Math.random() * (max - min + 1) + min);
+    for (let i = 0; i < qty; i++) {
+      let index = ~~(Math.random() * letters.length);
+      str.push(letters[index]);
+      total++;
+    }
+    str.push(' ');
+    total++;
+  }
+  str.pop();
+  return str;
 };
 
 module.exports = {
