1d5e11090a82137d94f99fd31c25719fe9c19e75
Alexei Sorokine <2picae@gmail.com>
2019-03-18 08:32:33 -0700

    Refactor key handling functions

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -14,76 +14,80 @@ class Exercise extends React.Component {
       test: false,
       next: false
     };
+    this.time = 0;
+    this.start = 0;
+    this.end = 0;
+
     this.startExercise = this.startExercise.bind(this);
+    this.displayPressedKey = this.displayPressedKey.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   startExercise() {
-    // let node = document.getElementById('exercise');
-    // console.log('NODE ', node)
-    // node.innerHTML = '';
     const test = loadTest(this.props.data);
     this.setState({testString: test, index: 0});
-    let time = 0;
-    let start = 0;
-    let end = 0;
-    let current = null;
 
-    const handleKey = (e) => {
-      if (this.state.next && e.type === 'keydown') {
+    if (!this.state.test) {
+      window.addEventListener('keydown', this.handleKeyDown);
+      window.addEventListener('keyup', this.handleKeyUp);
+      this.setState({test: true});
+    }
+  }
+
+  handleKeyUp(e) {
+    this.start = new Date();
+    console.log('key press time - ', this.start - this.end)
+  }
+
+  handleKeyDown(e) {
+    const pressedKey = e.key;
+    if (pressedKey === 'Escape') {
+      window.removeEventListener('keydown', this.handleKeyDown);
+      window.removeEventListener('keyup', this.handleKeyUp);
+      this.setState({test: false});
+    }
+    if (this.state.next) {
         this.setState({next: false});
         this.startExercise();
         return;
       }
-      let index = this.state.index;
-      const key = e.key;
-      if (key === 'Escape') {
-        window.removeEventListener('keydown', handleKey);
-        window.removeEventListener('keyup', handleKey);
-        this.setState({test: false});
-      }
-      if  (e.type === 'keyup') {
-        start = new Date();
-        console.log('key press time - ', start - end)
-      } else if (e.type === 'keydown') {
-        end = new Date();
-        time = end - start;
-        console.log('transition time - ', time);
-        if (letters.includes(key)) {
-          current = document.getElementById(key);
-          current.style.cssText = `background-color: ${this.state.colorPressed}`;
-          ((c) => {
-            setTimeout(() => {
-              c.style.cssText = `background-color: ${this.state.colorNormal}`;
-            }, 250);
-          })(current);
-        }
-        console.log('CURRENT KEY - ', key)
-        let correctKey = this.state.testString[this.state.index];
-        let letter = document.getElementById('index' + this.state.index + correctKey);
-        console.log('index: ', this.state.index, '; key: ', key, '; expected key: ', correctKey)
-        if (key === this.state.testString[this.state.index]) {
-          letter.className = 'used';
-          // letter.style.cssText = `background-color: ${this.state.colorUsed}`;
-          index++;
-          if (index === this.state.testString.length) {
-            this.setState({next: true});
-            return;
-          }
-          correctKey = this.state.testString[index];
-          letter = document.getElementById('index' + index + correctKey);
-          letter.className = 'cursor';
-          this.setState({index: index});
-        } else {
-          console.log('index - ', index)
-          letter.className = 'mistake';
-        }
-      }
-    };
-    if (!this.state.test) {
-      window.addEventListener('keydown', handleKey.bind(this));
-      window.addEventListener('keyup', handleKey.bind(this));
-      this.setState({test: true});
+    let index = this.state.index;
+    this.end = new Date();
+    this.time = this.end - this.start;
+    console.log('transition time - ', this.time);
+    if (letters.includes(pressedKey)) {
+      this.displayPressedKey(pressedKey);
     }
+    console.log('CURRENT KEY - ', pressedKey)
+    let expectedKey = this.state.testString[this.state.index];
+    let letter = document.getElementById('index' + this.state.index + expectedKey);
+    console.log('index: ', this.state.index, '; key: ', pressedKey, '; expected key: ', expectedKey)
+    if (pressedKey === this.state.testString[this.state.index]) {
+      letter.className = 'used';
+      index++;
+      if (index === this.state.testString.length) {
+        this.setState({next: true});
+        return;
+      }
+      expectedKey = this.state.testString[index];
+      letter = document.getElementById('index' + index + expectedKey);
+      letter.className = 'cursor';
+      this.setState({index: index});
+    } else {
+      console.log('index - ', index)
+      letter.className = 'mistake';
+    }
+  }
+
+  displayPressedKey(key) {
+    console.log('KEY: ', key);
+    let pressed = document.getElementById(key);
+    console.log('PRESSED: ', pressed);
+    pressed.style.cssText = `background-color: ${this.state.colorPressed}`;
+    setTimeout(() => {
+      pressed.style.cssText = `background-color: ${this.state.colorNormal}`;
+    }, 250);
   }
 
   displayString(flag) {
