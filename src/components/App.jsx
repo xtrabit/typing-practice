efdc7735975680818fb952167f6cab10f52243fa
Alexei Sorokine <2picae@gmail.com>
2019-04-08 14:49:59 -0700

    Refactor App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,73 +1,16 @@
 import React from 'react';
 import './styles.css';
-import Board from './Board';
-import Exercise from './Exercise';
-
+import ViewBoard from './ViewBoard';
+import Exercise from '../containers/exerciseContainer';
+import listenToKeys from '../lib/listenToKeys';
 import Header from './Header';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: 'anonymous',
-      data: null,
-      wpm: 0
-    };
-    this.loadData = this.loadData.bind(this);
-    this.writeData = this.writeData.bind(this);
-    this.setWpm = this.setWpm.bind(this);
-  }
-
-  componentDidMount() {
-    this.loadData();
-  }
-
-  loadData() {
-    fetch(`/load/${this.state.user}`)
-      .then(res => res.json())
-      .then(res => {
-        console.log('LOADED DATA - ', res);
-        res[0] === 'empty' ? this.setState({data: null}) : this.setState({data: res});
-      })
-      .catch(err => console.error(err));
-  }
-
-  writeData(data, cb) {
-    fetch(`/write/${this.state.user}`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(res => cb())
-      .catch(err => console.error(err));
-    let newData = this.state.data ? [...this.state.data] : [];
-    newData = newData.concat(data);
-    this.setState({data: newData});
-  }
-
-  setWpm(wpm) {
-    this.setState({wpm});
-  }
-
-  render() {
-    return (
-      <div className='app-wrapper'>
-        <Header
-          user={this.state.user}
-          wpm={this.state.wpm}
-        />
-        <Exercise
-          data={this.state.data}
-          user={this.state.user}
-          loadData={this.loadData}
-          writeData={this.writeData}
-          setWpm={this.setWpm}
-        />
-        <Board />
-      </div>
-    );
-  }
-}
+const App = ({pressedKeys}) => (
+  <div className='app-wrapper'>
+    <Header />
+    <Exercise listenToKeys={listenToKeys} />
+    <ViewBoard pressedKeys={pressedKeys} />
+  </div>
+);
 
 export default App;
