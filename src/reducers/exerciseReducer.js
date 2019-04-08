a32229af1ee7ca26f81ebd46ed1b25cc98f971ff
Alexei Sorokine <2picae@gmail.com>
2019-04-08 14:54:38 -0700

    Add exercise Reducer

---
+++ src/reducers/exerciseReducer.js
@@ -0,0 +1,14 @@
+import Redux from 'redux';
+
+const exerciseReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_EXERCISE':
+      return action.exercise;
+    case 'CLEAR_EXERCISE':
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default exerciseReducer;
